3628059af099cec01a00574b28c5869bea02949f
Yuval66068 <92807848+Yuval66068@users.noreply.github.com>
2021-10-19 18:30:34 +0300

    Add Initial Website Files

---
+++ index.js
@@ -0,0 +1,12 @@
+var i=0;
+var j=0;
+function textChange(){
+  var divtext=document.getElementById("#myid");
+  var divtext=document.getElementById("#myid2");
+  var divtext=document.getElementById("#myid3");
+  i=(i+1)%divtext.length;
+
+}
+set(setInterval(divtext () {
+
+}, 2000);)
